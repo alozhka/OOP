76ec2077df8ebf5bd1a4ac8b29f127c0dccbf136
Sergey <100339481+alozhka@users.noreply.github.com>
2025-02-15 10:59:46 +0300

    refactor

--- lw1/replace/main.cpp
+++ lw1/replace/main.cpp
@@ -6,10 +6,10 @@ const std::string HELP_IDENTIFIER = "-h";
 
 struct Input
 {
-  std::string inFile, outFile, searchString, replaceString;
+  std::string inFile, outFile, searchString, replaceString, signleLine;
 };
 
-void ParseInput(const char *argv[], Input &input)
+void ParseInputFromCommandLine(const char *argv[], Input &input)
 {
   input.inFile = argv[1];
   input.outFile = argv[2];
@@ -17,35 +17,46 @@ void ParseInput(const char *argv[], Input &input)
   input.replaceString = argv[4];
 }
 
-void ParseInputFromStdIn(Input &input)
+bool TryParseInputFromStdIn(Input &input)
 {
   std::cin >> input.searchString;
   std::cin >> input.replaceString;
+  std::cin >> input.signleLine;
+  return !std::cin.eof();
 }
 
-bool Replace(
-  const std::string &inFilename, const std::string &outFilename,
-  const std::string &searchStr, const std::string &replaceStr)
+std::string ReplaceSingleLine(std::string line, const std::string &searchStr, const std::string &replaceStr)
+{
+  size_t pos = 0;
+  while (pos = line.find(searchStr, pos), pos != std::string::npos)
+  {
+    line.replace(pos, searchStr.length(), replaceStr);
+    pos += replaceStr.length();
+  }
+
+  return line;
+}
+
+bool TryReplaceInFile(
+  const std::string &inFilename,
+  const std::string &outFilename,
+  const std::string &searchStr,
+  const std::string &replaceStr
+)
 {
   std::ifstream inFile(inFilename);
   if (!inFile.is_open())
   {
-    std::cerr << "Ошибка при открытии входного файла\n";
     return false;
   }
   std::ofstream outFile(outFilename);
 
-  size_t pos = 0;
   std::string line;
   while (!inFile.eof())
   {
     getline(inFile, line);
-    while (pos = line.find(searchStr, pos), pos != std::string::npos)
-    {
-      line.replace(pos, searchStr.length(), replaceStr);
-      pos += replaceStr.length();
-    }
-    outFile << line;
+    std::string replacedLine = ReplaceSingleLine(line, searchStr, replaceStr);
+    outFile << replacedLine;
   }
 
   inFile.close();
@@ -61,25 +72,32 @@ int main(const int argc, const char *argv[])
 
   if (argc == 1)
   {
-    ParseInputFromStdIn(input);
-    Replace(input.searchString, input.replaceString);
-  } else
+    if(!TryParseInputFromStdIn(input))
+    {
+      std::cout << "ERROR\n";
+      return 0;
+    }
+    std::cout << ReplaceSingleLine(input.signleLine, input.searchString, input.replaceString);
+  }
+  else
   {
     if (argv[1] == HELP_IDENTIFIER)
     {
-      std::cout << "Usage\n";
+      std::cout << "Usage: replace <input_file> <output_file> <search_string> <replace_string>\n";
       return 0;
     }
-    if (argc < 5)
+    if (argc < ARGS_AMOUNT)
     {
       std::cout << "ERROR\n";
       return 1;
     }
-    ParseInput(argv, input);
-    if (!Replace(input.inFile, input.outFile, input.searchString, input.replaceString))
+    ParseInputFromCommandLine(argv, input);
+    if (!TryReplaceInFile(input.inFile, input.outFile, input.searchString, input.replaceString))
     {
+      std::cout << "ERROR\n";
       return 1;
     }
   }
+
   return 0;
 }
