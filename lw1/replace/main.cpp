dd9bd2dd58bd4b95f0f8247204307483fb4b6277
Sergey <100339481+alozhka@users.noreply.github.com>
2025-02-20 09:32:57 +0300

    refactor: убрал лишнюю функцию по правкам
    сдал на 0.9 => 68

--- lw1/replace/main.cpp
+++ lw1/replace/main.cpp
@@ -1,68 +1,67 @@
 #include <fstream>
 #include <iostream>
-#include <vector>
 
 constexpr ushort ARGS_AMOUNT = 5;
 const std::string HELP_IDENTIFIER = "-h";
 
 struct Input
 {
-  std::string inFile, outFile, searchString, replaceString;
+	std::string inFile, outFile, searchString, replaceString;
 };
 
-void ParseInputFromCommandLine(const char *argv[], Input &input)
+void ParseInputFromCommandLine(const char* argv[], Input& input)
 {
-  input.inFile = argv[1];
-  input.outFile = argv[2];
-  input.searchString = argv[3];
-  input.replaceString = argv[4];
+	input.inFile = argv[1];
+	input.outFile = argv[2];
+	input.searchString = argv[3];
+	input.replaceString = argv[4];
 }
 
-bool TryParseInputFromStdIn(Input &input)
+bool TryParseInputFromStdIn(Input& input)
 {
-  if (!std::getline(std::cin, input.searchString))
-  {
-    return false;
-  }
-  if (!std::getline(std::cin, input.replaceString))
-  {
-    return false;
-  }
+	if (!std::getline(std::cin, input.searchString))
+	{
+		return false;
+	}
+	if (!std::getline(std::cin, input.replaceString))
+	{
+		return false;
+	}
 
-  return !std::cin.eof();
+	return !std::cin.eof();
 }
 
-std::string CopyStringWithReplacement(const std::string &line, const std::string &searchStr, const std::string &replaceStr)
+std::string CopyStringWithReplacement(const std::string& line, const std::string& searchStr, const std::string& replaceStr)
 {
-  size_t pos = 0;
-  std::string result;
+	if (searchStr.empty())
+	{
+		return line;
+	}
 
-  if (searchStr.empty())
-  {
-    return line;
-  }
+	size_t pos = 0;
+	std::string result;
 
-  while (pos < line.size())
-  {
-    const size_t foundPos = line.find(searchStr, pos);
-    if (foundPos < line.size())
-    {
-      result.append(line, pos, foundPos - pos);
-      result.append(replaceStr);
-      pos = foundPos + searchStr.size();
-    } else
-    {
-      result.append(line, pos);
-      pos = std::string::npos;
-    }
-  }
-  return result;
+	while (pos < line.size())
+	{
+		const size_t foundPos = line.find(searchStr, pos);
+		if (foundPos < line.size())
+		{
+			result.append(line, pos, foundPos - pos);
+			result.append(replaceStr);
+			pos = foundPos + searchStr.size();
+		}
+		else
+		{
+			result.append(line, pos);
+			pos = std::string::npos;
+		}
+	}
+	return result;
 }
 
 void CopyStreamWithReplacement(
-	std::istream &input, std::ostream &output,
-	const std::string &searchStr, const std::string &replaceStr
-)
+	std::istream& input, std::ostream& output,
+	const std::string& searchStr, const std::string& replaceStr)
 {
 	std::string line;
 	while (getline(input, line))
@@ -72,33 +71,9 @@ void CopyStreamWithReplacement(
 	}
 }
 
-bool TryCopyFileWithReplacement(
-  const std::string &inFilename, const std::string &outFilename,
-  const std::string &searchStr, const std::string &replaceStr
-)
-{
-  std::ifstream inFile(inFilename);
-  if (!inFile.is_open())
-  {
-    return false;
-  }
-  std::ofstream outFile(outFilename);
-  if (!outFile.is_open())
-  {
-    return false;
-  }
-
-  CopyStreamWithReplacement(inFile, outFile, searchStr, replaceStr);
-
-  inFile.close();
-  outFile.close();
-  return true;
-}
-
-
 void PrintHelp()
 {
-  std::cout << "Usage: replace <input_file> <output_file> <search_string> <replace_string>\n";
+	std::cout << "Usage: replace <input_file> <output_file> <search_string> <replace_string>\n";
 }
 
 void ReplaceFromStdin()
@@ -113,41 +88,57 @@ void ReplaceFromStdin()
 	CopyStreamWithReplacement(std::cin, std::cout, input.searchString, input.replaceString);
 }
 
-bool TryReplaceFromFile(const char *argv[])
+bool TryReplaceFromFile(const char* argv[])
 {
-  Input input;
+	Input input;
+	ParseInputFromCommandLine(argv, input);
 
-  ParseInputFromCommandLine(argv, input);
-  if (!TryCopyFileWithReplacement(input.inFile, input.outFile, input.searchString, input.replaceString))
-  {
-    std::cout << "ERROR\n";
-    return false;
-  }
+	std::ifstream inFile(input.inFile);
+	if (!inFile.is_open())
+	{
+		return false;
+	}
+	std::ofstream outFile(input.outFile);
+	if (!outFile.is_open())
+	{
+		return false;
+	}
 
-  return true;
+	CopyStreamWithReplacement(inFile, outFile, input.searchString, input.replaceString);
+
+	inFile.close();
+	outFile.close();
+
+	return true;
 }
 
-int main(const int argc, const char *argv[])
+int main(const int argc, const char* argv[])
 {
-  setlocale(LC_ALL, "rus");
+	setlocale(LC_ALL, "rus");
 
-  if (argc == 1)
-  {
-  	ReplaceFromStdin();
-  	return 0;
-  }
+	if (argc == 1)
+	{
+		ReplaceFromStdin();
+		return 0;
+	}
 
-  if (argv[1] == HELP_IDENTIFIER)
-  {
-    PrintHelp();
-    return 0;
-  }
+	if (argv[1] == HELP_IDENTIFIER)
+	{
+		PrintHelp();
+		return 0;
+	}
 
-  if (argc < ARGS_AMOUNT)
-  {
-    std::cout << "ERROR\n";
-    return 1;
-  }
+	if (argc < ARGS_AMOUNT)
+	{
+		std::cout << "ERROR\n";
+		return 1;
+	}
 
-  return !TryReplaceFromFile(argv);
+	if (!TryReplaceFromFile(argv))
+	{
+		std::cout << "ERROR\n";
+		return 1;
+	}
+
+	return 0;
 }
