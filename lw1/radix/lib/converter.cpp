68c67cd7c73bf46a5902f95fbe7737ad93f231fb
Sergey <100339481+alozhka@users.noreply.github.com>
2025-02-23 19:59:49 +0300

    feat: сделал перевод строки в десятичное число, почти покрыл тестами

--- lw1/radix/lib/converter.cpp
+++ lw1/radix/lib/converter.cpp
@@ -1,11 +1,78 @@
+#include <format>
 #include <stdexcept>
+
+int CharToInt(char ch)
+{
+	if ('0' <= ch && ch <= '9')
+	{
+		return ch - '0';
+	}
+	if ('A' <= ch && ch <= 'Z')
+	{
+		return ch - 'A' + 10;
+	}
+	if ('a' <= ch && ch <= 'z')
+	{
+		return ch - 'a' + 10;
+	}
+
+	throw std::invalid_argument("The number must be in the segments [0, 10] and [A, Z]");
+}
+
+void ThrowIfReachesOverflow(const bool isNegative, const int result, const int radix, const int number)
+{
+	if (isNegative)
+	{
+		if (-result < (std::numeric_limits<int>::min() + number) / radix)
+		{
+			throw std::overflow_error("Reached low overflow");
+		}
+	}
+	else
+	{
+		if (result > (std::numeric_limits<int>::max() - number) / radix)
+		{
+			throw std::overflow_error("Reached top overflow");
+		}
+	}
+}
 
 int StringToInt(const std::string& str, int radix)
 {
-	return 10;
+	if (radix < 2 || radix > 36)
+	{
+		throw std::invalid_argument("The base of the number system should be in the range from 2 to 36");
+	}
+	bool isNegative = false;
+	size_t i = 0;
+	int result = 0;
+
+	if (str[0] == '-')
+	{
+		isNegative = true;
+		i = 1;
+	}
+
+	for (; i < str.size(); i++)
+	{
+		int decimalDigit = CharToInt(str[i]);
+		if (decimalDigit >= radix)
+		{
+			throw std::runtime_error(std::format("The number must be with base {}, got {}", radix, decimalDigit));
+		}
+		ThrowIfReachesOverflow(isNegative, result, radix, decimalDigit);
+		result = result * radix + decimalDigit;
+	}
+
+	return isNegative ? -result : result;
 }
 
 std::string IntToString(int n, int radix)
 {
+	if (radix < 2 || radix > 36)
+	{
+		throw std::invalid_argument("The base of the number system should be in the range from 2 to 36");
+	}
+
 	throw std::invalid_argument("Not implemented");
 }
--- lw1/radix/tests/unit_tests.cpp
+++ lw1/radix/tests/unit_tests.cpp
@@ -2,17 +2,32 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
 
-TEST_CASE("Converts string to int with numeric system", "[positive][converter]")
+struct TestData
 {
-	struct TestData
-	{
-		std::string str;
-		int radix;
-		int actual;
-	};
+	std::string str;
+	int radix;
+	int actual;
+};
 
-	auto testData = GENERATE(
-		TestData{ "10", 10, 10 });
+TEST_CASE("Converts string to int with numeric system", "[positive]")
+{
+	TestData testData = GENERATE(
+		TestData{ "10", 10, 10 },
+		TestData{ "10", 16, 16 },
+		TestData{ "ZZ", 36, 1295 },
+		TestData{ "0", 36, 0 },
+		TestData{ "2147483647", 10, 2147483647 },
+		TestData{ "-2147483648", 10, -2147483648 });
 
 	REQUIRE(StringToInt(testData.str, testData.radix) == testData.actual);
+}
+
+TEST_CASE("Handles overflow", "[negative]")
+{
+
+	TestData testData = GENERATE(
+		TestData{ "2147483648", 10 },
+		TestData{ "-2147483649", 10 });
+
+	REQUIRE_THROWS(StringToInt(testData.str, testData.radix), "Reached overflow");
 }
