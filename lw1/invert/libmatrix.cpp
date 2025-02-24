3936c182781fb417177671e3bfb1fbbf8e115bc7
Sergey <100339481+alozhka@users.noreply.github.com>
2025-02-24 23:14:40 +0300

    feat: расписал парсинг из разных источников и вывод help

--- lw1/invert/libmatrix.cpp
+++ lw1/invert/libmatrix.cpp
@@ -1,4 +1,16 @@
 #include <array>
+#include <ostream>
 
 using matrix = std::array<std::array<int, 3>, 3>;
 
+void PrintMatrix(std::ostream& out, const matrix m)
+{
+	for (const std::array<int, 3>& row : m)
+	{
+		for (const int cell : row)
+		{
+			out << cell << "\t";
+		}
+		out << std::endl;
+	}
+}
