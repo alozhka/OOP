aa4306daf071ff094c1d0b30ca0535fdea8696bd
Sergey <100339481+alozhka@users.noreply.github.com>
2025-02-27 10:07:12 +0300

    refactor: вынес проверку на значения radix и вынес символьные константы

--- lw1/radix/lib/converter.cpp
+++ lw1/radix/lib/converter.cpp
@@ -1,19 +1,27 @@
 #include <format>
 #include <stdexcept>
 
+// перенести числа в константы, вынести проверку на radix в отдельную функцию
+constexpr char CHAR_0 = '0';
+constexpr char CHAR_9 = '9';
+constexpr char CHAR_UPPER_A = 'A';
+constexpr char CHAR_UPPER_Z = 'Z';
+constexpr char CHAR_A = 'a';
+constexpr char CHAR_Z = 'z';
+
 int CharToInt(const char ch)
 {
-	if ('0' <= ch && ch <= '9')
+	if (CHAR_0 <= ch && ch <= CHAR_9)
 	{
-		return ch - '0';
+		return ch - CHAR_0;
 	}
-	if ('A' <= ch && ch <= 'Z')
+	if (CHAR_UPPER_A <= ch && ch <= CHAR_UPPER_Z)
 	{
-		return ch - 'A' + 10;
+		return ch - CHAR_UPPER_A + 10;
 	}
-	if ('a' <= ch && ch <= 'z')
+	if (CHAR_A <= ch && ch <= CHAR_Z)
 	{
-		return ch - 'a' + 10;
+		return ch - CHAR_A + 10;
 	}
 
 	throw std::invalid_argument("The digit must be in the segments [0, 10] and [A, Z]");
@@ -23,15 +31,23 @@ char IntToChar(const int i)
 {
 	if (0 <= i && i <= 9)
 	{
-		return '0' + i;
+		return CHAR_0 + i;
 	}
 
 	if (10 <= i && i <= 35)
 	{
-		return 'A' + i - 10;
+		return CHAR_UPPER_A + i - 10;
 	}
 
 	throw std::invalid_argument("Integer has to be in range from 0 to 35");
+}
+
+void ThrowIfRadixOutOfRange(int radix)
+{
+	if (radix < 2 || radix > 36)
+	{
+		throw std::invalid_argument("The radix should be in the range from 2 to 36");
+	}
 }
 
 void ThrowIfReachesOverflow(const bool isNegative, const int result, const int radix, const int number)
@@ -54,10 +70,7 @@ void ThrowIfReachesOverflow(const bool isNegative, const int result, const int r
 
 int StringToInt(const std::string& str, const int radix)
 {
-	if (radix < 2 || radix > 36)
-	{
-		throw std::invalid_argument("The radix should be in the range from 2 to 36");
-	}
+	ThrowIfRadixOutOfRange(radix);
 	bool isNegative = false;
 	size_t i = 0;
 	int result = 0;
@@ -70,7 +83,7 @@ int StringToInt(const std::string& str, const int radix)
 
 	for (; i < str.size(); i++)
 	{
-		int decimalDigit = CharToInt(str[i]);
+		const int decimalDigit = CharToInt(str[i]);
 		if (decimalDigit >= radix)
 		{
 			throw std::runtime_error(std::format("The number must be with base {}, got {}", radix, IntToChar(decimalDigit)));
@@ -84,10 +97,7 @@ int StringToInt(const std::string& str, const int radix)
 
 std::string IntToString(int n, const int radix)
 {
-	if (radix < 2 || radix > 36)
-	{
-		throw std::invalid_argument("The base of the number system should be in the range from 2 to 36");
-	}
+	ThrowIfRadixOutOfRange(radix);
 	if (n == 0)
 	{
 		return "0";
