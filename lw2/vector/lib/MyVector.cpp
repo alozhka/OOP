8212947202062d47a00fb2402e10f1a6d432e9ec
sergei.lozhkin <alozhka@yahoo.com>
2025-03-06 00:33:47 +0300

    feat: добавил проверку на невалидное число

--- lw2/vector/lib/MyVector.cpp
+++ lw2/vector/lib/MyVector.cpp
@@ -9,6 +9,11 @@ void ReadNumbers(std::istream& in, std::vector<double>& v)
 	{
 		v.push_back(n);
 	}
+
+	if (!in.eof() && in.fail())
+	{
+		throw std::invalid_argument("ERROR");
+	}
 }
 void ProcessNumbers(const std::vector<double>& v)
 {
--- lw2/vector/tests/unit_tests.cpp
+++ lw2/vector/tests/unit_tests.cpp
@@ -1,6 +1,13 @@
 #include "../lib/MyVector.h"
+#include "catch2/matchers/catch_matchers_exception.hpp"
+
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
 #include <fstream>
+
+/**
+ * ReadNumbers
+ */
 
 TEST_CASE("Read numbers from stream", "[read][positive]")
 {
@@ -12,4 +19,14 @@ TEST_CASE("Read numbers from stream", "[read][positive]")
 	in.close();
 
 	REQUIRE(expected == actual);
+}
+
+// negative
+
+TEST_CASE("Cannot read invalid number format", "[read][negative]")
+{
+	std::vector<double> actual;
+	std::ifstream in("../tests/invalid/not-number.txt");
+
+	REQUIRE_THROWS_MATCHES(ReadNumbers(in, actual), std::invalid_argument, Catch::Matchers::Message("ERROR"));
 }
