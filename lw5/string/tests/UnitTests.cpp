06fec21ea464e50090b4f930f2286da6a38b7313
sergei.lozhkin <alozhka@yahoo.com>
2025-04-29 00:23:58 +0300

    feat: расписал костяк для строки

---
+++ lw5/string/tests/UnitTests.cpp
@@ -0,0 +1,13 @@
+#include <CString.h>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Can construct string", "[UnitTests]")
+{
+	SECTION("Default constructor")
+	{
+		CString str;
+		CHECK(str.GetCapacity() == 1);
+		CHECK(str.GetLength() == 0);
+		CHECK(std::strcmp(str.GetStringData(), "\0") == 0);
+	}
+}
