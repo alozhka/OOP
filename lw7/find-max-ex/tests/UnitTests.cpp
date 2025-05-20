8f920d4fd605c933a5fc407ec10f9492b832867e
Sergey Lozhkin <alozhka@yahoo.com>
2025-05-20 19:42:35 +0300

    tests: пишу тест на сравнение

--- lw7/find-max-ex/tests/UnitTests.cpp
+++ lw7/find-max-ex/tests/UnitTests.cpp
@@ -5,23 +5,46 @@ struct Athlete
 {
 	std::string name{};
 	ushort age = 0;
+	ushort weight = 0;
+
+	bool operator==(const Athlete& other) const
+	{
+		return name == other.name && age == other.age && weight == other.weight;
+	}
 };
 
 namespace TestData
 {
-std::function AthleteAgeComparator = [](const Athlete& a, const Athlete& b) { return a.age < b.age; };
+std::function AthleteMaxAgeComparator = [](const Athlete& a, const Athlete& b) { return a.age > b.age; };
+std::function AthleteMaxWeightComparator = [](const Athlete& a, const Athlete& b) { return a.weight > b.weight; };
+std::function AthleteMaxNameComparator = [](const Athlete& a, const Athlete& b) { return a.name > b.name; };
+
+Athlete Alex = { "Александр", 22, 79 };
+Athlete Vladimir = { "Владимир", 30, 70 };
+Athlete Yaroslav = { "Ярослав", 19, 66 };
+std::vector Athletes = { Alex, Vladimir, Yaroslav };
 } // namespace TestData
 
 TEST_CASE("Finds max element by predicate", "[compare]")
 {
+	Athlete max{};
+
+	CHECK(CompareUtils::FindMax(TestData::Athletes, max, TestData::AthleteMaxAgeComparator));
+	CHECK(max == TestData::Vladimir);
+
+	CHECK(CompareUtils::FindMax(TestData::Athletes, max, TestData::AthleteMaxNameComparator));
+	CHECK(max == TestData::Yaroslav);
+
+	CHECK(CompareUtils::FindMax(TestData::Athletes, max, TestData::AthleteMaxWeightComparator));
+	CHECK(max == TestData::Alex);
 }
 
 // negative
-TEST_CASE("Cannot find max element if vector is empty", "[compare]")
+TEST_CASE("Cannot find max element if vector is empty", "[compare][empty]")
 {
 	std::vector<Athlete> athletes{};
 	Athlete* max = nullptr;
 
-	CHECK(!CompareUtils::FindMax(athletes, *max, TestData::AthleteAgeComparator));
+	CHECK(!CompareUtils::FindMax(athletes, *max, TestData::AthleteMaxAgeComparator));
 	CHECK(max == nullptr);
 }
