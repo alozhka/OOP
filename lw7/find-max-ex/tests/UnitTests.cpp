46ca80ad0a87c1a0cc85670dba47614bd8a9a64c
Sergey Lozhkin <alozhka@yahoo.com>
2025-05-20 15:30:26 +0300

    tests: написал негативный тест на пустой список

--- lw7/find-max-ex/tests/UnitTests.cpp
+++ lw7/find-max-ex/tests/UnitTests.cpp
@@ -1,12 +1,27 @@
+#include "../src/CompareUtils.h"
 #include "catch2/catch_test_macros.hpp"
 
 struct Athlete
 {
-	std::string lastName, firstName;
-	
+	std::string name{};
+	ushort age = 0;
 };
+
+namespace TestData
+{
+std::function AthleteAgeComparator = [](const Athlete& a, const Athlete& b) { return a.age < b.age; };
+} // namespace TestData
 
 TEST_CASE("Finds max element by predicate", "[compare]")
 {
+}
 
+// negative
+TEST_CASE("Cannot find max element if vector is empty", "[compare]")
+{
+	std::vector<Athlete> athletes{};
+	Athlete* max = nullptr;
+
+	CHECK(!CompareUtils::FindMax(athletes, *max, TestData::AthleteAgeComparator));
+	CHECK(max == nullptr);
 }
