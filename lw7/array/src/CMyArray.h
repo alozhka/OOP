c16ea44657066abef7b1a0b7cbd57eef54fa5d26
Sergey Lozhkin <alozhka@yahoo.com>
2025-05-22 00:50:25 +0300

    feat: копирующий конструктор

---
+++ lw7/array/src/CMyArray.h
@@ -0,0 +1,131 @@
+#pragma once
+#include <iterator>
+
+template <typename T>
+class CMyArray
+{
+public:
+	CMyArray() = default;
+	//~CMyArray() noexcept;
+
+	CMyArray(const CMyArray& other);
+	// CMyArray(CMyArray&& other) noexcept;
+	// CMyArray& operator=(const CMyArray& other);
+	// CMyArray& operator=(CMyArray&& other) noexcept;
+
+	void PushBack(const T& item);
+	[[nodiscard]] size_t Size() const noexcept;
+	[[nodiscard]] size_t Capacity() const noexcept;
+	// void Resize();
+	void Clear();
+
+	// T& operator[](size_t index);
+	// const T& operator[](size_t index) const;
+
+	using Iterator = T*;
+	using ConstIterator = const T*;
+	using ReverseIterator = std::reverse_iterator<Iterator>;
+	using ConstReverseIterator = const std::reverse_iterator<Iterator>;
+
+	Iterator begin() const noexcept;
+	Iterator end() const noexcept;
+	// ReverseIterator rbegin() noexcept;
+	// ReverseIterator rend() noexcept;
+
+private:
+	void EnsureCapacity(size_t size);
+	void IncreaseCapacity(size_t size);
+
+	T* m_data = nullptr;
+	size_t m_size = 0;
+	size_t m_capacity = 0;
+};
+
+//-----------------Implementation-----------------
+
+template <typename T>
+CMyArray<T>::CMyArray(const CMyArray& other)
+	: m_size(other.m_size)
+	, m_capacity(other.m_capacity)
+{
+	T* temp = new T[other.m_size];
+	try
+	{
+		std::copy(other.m_data, other.m_data + other.m_size, temp);
+	}
+	catch (...)
+	{
+		delete[] temp;
+		throw;
+	}
+
+	delete[] m_data;
+	m_data = temp;
+}
+
+template <typename T>
+void CMyArray<T>::PushBack(const T& item)
+{
+	EnsureCapacity(m_size + 1);
+	m_data[m_size++] = item;
+}
+
+template <typename T>
+size_t CMyArray<T>::Size() const noexcept
+{
+	return m_size;
+}
+
+template <typename T>
+size_t CMyArray<T>::Capacity() const noexcept
+{
+	return m_capacity;
+}
+
+template <typename T>
+void CMyArray<T>::Clear()
+{
+	delete[] m_data;
+	m_data = nullptr;
+	m_size = 0;
+	m_capacity = 0;
+}
+
+template <typename T>
+typename CMyArray<T>::Iterator CMyArray<T>::begin() const noexcept
+{
+	return m_data;
+}
+
+template <typename T>
+typename CMyArray<T>::Iterator CMyArray<T>::end() const noexcept
+{
+	return m_data + m_size;
+}
+
+template <typename T>
+void CMyArray<T>::EnsureCapacity(size_t size)
+{
+	if (size <= m_capacity)
+	{
+		return;
+	}
+
+	IncreaseCapacity(size);
+}
+
+template <typename T>
+void CMyArray<T>::IncreaseCapacity(size_t size)
+{
+	const size_t newCapacity = std::max(size, m_capacity ? m_capacity * 2 : 1);
+	T* newData = new T[newCapacity];
+
+	for (size_t i = 0; i < m_size; ++i)
+	{
+		newData[i] = std::move(m_data[i]);
+	}
+
+	delete[] m_data;
+	m_data = newData;
+	m_capacity = newCapacity;
+}
--- lw7/array/tests/UnitTests.cpp
+++ lw7/array/tests/UnitTests.cpp
@@ -1,6 +1,54 @@
+#include "../src/CMyArray.h"
+
 #include <catch2/catch_test_macros.hpp>
+
+constexpr size_t DEFAULT_CAPACITY = 10;
+
+template <typename T>
+void CompareArrayAndVector(const CMyArray<T>& left, const std::vector<T>& right)
+{
+	CHECK(left.Size() == right.size());
+
+	size_t i = 0;
+	for (T value : left)
+	{
+		CHECK(value == right[i++]);
+	}
+}
+
+template <typename T>
+void CompareArrays(const CMyArray<T>& left, const CMyArray<T>& right)
+{
+	CHECK(left.Size() == right.Size());
+	CHECK(left.Capacity() == right.Capacity());
+
+	auto leftIt = left.begin();
+	for (T value : right)
+	{
+		CHECK(*leftIt == value);
+		++leftIt;
+	}
+}
 
 TEST_CASE("Constructs array", "[array][ctor]")
 {
-	CHECK(false);
+	SECTION("Default constructor does not allocate memory")
+	{
+		CMyArray<int> array{};
+
+		CHECK(0 == array.Size());
+		CHECK(0 == array.Capacity());
+	}
+
+	SECTION("Copy constructor")
+	{
+		CMyArray<int> array{};
+		array.PushBack(5);
+		array.PushBack(3);
+		array.PushBack(-99);
+
+		CMyArray copy(array);
+
+		CompareArrays(copy, array);
+	}
 }
